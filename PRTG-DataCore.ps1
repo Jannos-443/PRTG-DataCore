5f12fbdf8e5da05eda2e998ed753ab772b81f8a6
Jannos-443 <77551365+Jannos-443@users.noreply.github.com>
2021-12-09 21:52:03 +0100

    merge scripts

--- PRTG-DataCore.ps1
+++ PRTG-DataCore.ps1
@@ -1,9 +1,9 @@
 <#   
     .SYNOPSIS
-    Monitors DataCore Alerts
+    Monitors DataCore Status/Alerts/Pools/Ports/VirtualDisks
 
     .DESCRIPTION
-    Using DataCore Powershell commands this Script checks DataCore Alerts
+    Using DataCore Powershell Module this Script checks DataCore Status/Alerts/Pools/Ports/VirtualDisks
 
     .PARAMETER DcsServer
     The Hostname of the DataCore Server
@@ -16,8 +16,8 @@
     
     .EXAMPLE
     Sample call from PRTG EXE/Script Advanced
-    PRTG-DataCore-Alerts.ps1 -DcsServer 'YourDataCoreServer' (Windows Auth) <- better 
-    PRTG-DataCore-Alerts.ps1 -DcsServer 'YourDataCoreServer' -User 'YourUsername' -Password 'YourPassword' (Username and Password)
+    PRTG-DataCore-Alerts.ps1 -DcsServer 'YourDataCoreServer' -DcsStatus (Windows Auth) <- better 
+    PRTG-DataCore-Alerts.ps1 -DcsServer 'YourDataCoreServer' -DcsStatus -User 'YourUsername' -Password 'YourPassword' (Username and Password)
 
     Author:  Jannos-443
     https://github.com/Jannos-443/PRTG-DataCore
@@ -26,7 +26,14 @@
 param(
     [string]$DcsServer = '',
     [string]$User = '',
-    [string]$Password = ''
+    [string]$Password = '',
+    [switch]$DcsAlerts,
+    [switch]$DcsPools,
+    [switch]$DcsPorts,
+    [switch]$DcsVirtualDisks,
+    [switch]$DcsStatus,
+    [string]$Excludes = '',
+    [string]$Includes = ''
 )
 
 #Catch all unhandled Errors
@@ -38,9 +45,10 @@ trap{
     $Output = "line:$($_.InvocationInfo.ScriptLineNumber.ToString()) char:$($_.InvocationInfo.OffsetInLine.ToString()) --- message: $($_.Exception.Message.ToString()) --- line: $($_.InvocationInfo.Line.ToString()) "
     $Output = $Output.Replace("<","")
     $Output = $Output.Replace(">","")
+    $Output = $Output.Replace("#","")
     Write-Output "<prtg>"
     Write-Output "<error>1</error>"
-    Write-Output "<text>$Output</text>"
+    Write-Output "<text>$($Output)</text>"
     Write-Output "</prtg>"
     Exit
 }
@@ -78,6 +86,46 @@ if ($env:PROCESSOR_ARCHITEW6432 -eq "AMD64")
 #End
 #############################################################################    
 
+# Select Mode
+$ModeCount = 0
+if($DcsStatus)
+    {
+    $ModeCount ++
+    }
+if($DcsAlerts)
+    {
+    $ModeCount ++
+    }
+if($DcsPools)
+    {
+    $ModeCount ++
+    }
+if($DcsPorts)
+    {
+    $ModeCount ++
+    }
+if($DcsVirtualDisks)
+    {
+    $ModeCount ++
+    }
+if($ModeCount -eq 0)
+    {
+    Write-Output "<prtg>"
+    Write-Output "<error>1</error>"
+    Write-Output "<text>Please select scripte mode. e.g. -DcsStatus</text>"
+    Write-Output "</prtg>"
+    Exit  
+    }
+if($ModeCount -gt 1)
+    {
+    Write-Output "<prtg>"
+    Write-Output "<error>1</error>"
+    Write-Output "<text>Please only select ONE scripte mode at once. e.g. -DcsStatus</text>"
+    Write-Output "</prtg>"
+    Exit   
+    }
+
+
 # Error if there's anything going on
 $ErrorActionPreference = "Stop"
 
@@ -87,8 +135,8 @@ try {
     Import-Module "C:\Program Files\DataCore\Powershell Support\DataCore.Executive.Cmdlets.dll" -ErrorAction Stop
 } catch {
     Write-Output "<prtg>"
-    Write-Output " <error>1</error>"
-    Write-Output " <text>Error Loading DataCore Powershell Module ($($_.Exception.Message))</text>"
+    Write-Output "<error>1</error>"
+    Write-Output "<text>Error Loading DataCore Powershell Module ($($_.Exception.Message))</text>"
     Write-Output "</prtg>"
     Exit
 }
@@ -110,82 +158,432 @@ try {
 catch
     {
     Write-Output "<prtg>"
-    Write-Output " <error>1</error>"
-    Write-Output " <text>Could not connect to Datacore server $DcsServer. Error: $($_.Exception.Message)</text>"
+    Write-Output "<error>1</error>"
+    Write-Output "<text>Could not connect to Datacore server $($DcsServer). Error: $($_.Exception.Message)</text>"
     Write-Output "</prtg>"
     Exit
     }
 
-# Get DataCore Server Alerts
-$DCsAlerts = Get-DcsAlert -Connection $con
-
-$ErrorCount = 0
-$WarningCount = 0
-$ErrorText = "Error: "
-$WarningText = "Warning: "
-
-foreach($alert in $DCsAlerts)
-    {
-    if($alert.Level -eq "Warning")
-        {
-        $WarningCount += 1
-        $WarningText += "$($alert.MessageText) ###"
-        }
-    elseif($alert.Level -eq "Error")
-        {
-        $ErrorCount += 1
-        $ErrorText += "$($alert.MessageText) ###"
-        }
-    }
-
+# Output Text
+$OutputText = ""
 $xmlOutput = '<prtg>'
 
-$xmlOutput = $xmlOutput + "<result>
-        <channel>Error Count</channel>
-        <value>$($ErrorCount)</value>
-        <unit>Count</unit>
-        <limitmode>1</limitmode>
-        <LimitMaxError>0</LimitMaxError>
-        </result>
-        <result>
-        <channel>Warning Count</channel>
-        <value>$($WarningCount)</value>
-        <unit>Count</unit>
-        <limitmode>1</limitmode>
-        <LimitMaxWarning>0</LimitMaxWarning>
-        </result>"
+# Start Region Datacore Alerts
+if($DcsAlerts)
+    {
+    $Alerts = Get-DcsAlert -Connection $con
+
+    $ErrorCount = 0
+    $WarningCount = 0
+    $ErrorText = "Error: "
+    $WarningText = "Warning: "
+
+    foreach($alert in $Alerts)
+        {
+        if($alert.Level -eq "Warning")
+            {
+            $WarningCount += 1
+            $WarningText += "$($alert.MessageText) ###"
+            }
+        elseif($alert.Level -eq "Error")
+            {
+            $ErrorCount += 1
+            $ErrorText += "$($alert.MessageText) ###"
+            }
+        }
+
+    $xmlOutput = $xmlOutput + "<result>
+            <channel>Error Count</channel>
+            <value>$($ErrorCount)</value>
+            <unit>Count</unit>
+            <limitmode>1</limitmode>
+            <LimitMaxError>0</LimitMaxError>
+            </result>
+            <result>
+            <channel>Warning Count</channel>
+            <value>$($WarningCount)</value>
+            <unit>Count</unit>
+            <limitmode>1</limitmode>
+            <LimitMaxWarning>0</LimitMaxWarning>
+            </result>"
+
+
+
+
+    if($ErrorCount -gt 0)
+        {
+        $OutputText += "Error: $($ErrorText)"
+        }
+
+    if($WarningCount -gt 0)
+        {
+        $OutputText += "Warning: $($WarningText)"
+        }
+
+
+    if(($WarningCount -gt 0) -or ($ErrorCount -gt 0))
+        {
+        $OutputText = $OutputText.Replace("<","")
+        $OutputText = $OutputText.Replace(">","")
+        $OutputText = $OutputText.Replace("#","")
+        #The number sign (#) is not supported in sensor messages. If a message contains a number sign, the message is clipped at this point - https://www.paessler.com/manuals/prtg/custom_sensors
+        
+        $xmlOutput = $xmlOutput + "<text>$($OutputText)</text>"
+        }
+
+    else
+        {
+        $xmlOutput = $xmlOutput + "<text>No Alerts found</text>"
+        }
+    }
+# End Region Datacore Alerts
+
+
+#Start Region Datacore Status
+if($DcsStatus)
+    {
+    # Get DataCore Server
+    $DCs = Get-DcsServer -Connection $con
+
+    #Online Server
+    $DCsOnline = ($DCs | Where-Object {$_.State -eq "Online"}).count
+
+    $xmlOutput = $xmlOutput + "<result>
+            <channel>Server Online</channel>
+            <value>$($DCsOnline)</value>
+            <unit>Count</unit>
+            <limitmode>1</limitmode>
+            <LimitMinError>1</LimitMinError>
+            </result>"
+
+    # For Each Server
+    $NodeOnlineTXT = "Nodes Online: "
+    foreach ($DC in $DCs) 
+        {
+        if($DC.State -eq "Online")
+            {
+            $NodeOnlineTXT += "$($DC.Caption), "
+            }
+        #State
+        switch ($DC.State)
+            {
+            'NotPresent' { $dcstate = 1 }
+            'Offline' { $dcstate = 2}
+            'Online' { $dcstate = 3}
+            'Failed' { $dcstate = 4}
+            default { $dcstate = -1 }
+            }
+        $xmlOutput = $xmlOutput + "<result>
+            <channel>$($DC.Caption) State</channel>
+            <value>$($dcstate)</value>
+            <ValueLookup>prtg.datacore.state</ValueLookup>
+            </result>"
+
+        #LogStatus
+        switch ($DC.LogStatus)
+            {
+            'Operational' { $dclogstatus = 1 }
+            'StoragePaused' { $dclogstatus = 2}
+            'StorageFailed' { $dclogstatus = 3}
+            'QueueFull' { $dclogstatus = 4}
+            'Flooded' { $dclogstatus = 5}
+            default { $dclogstatus = -1 }
+            }
+        $xmlOutput = $xmlOutput + "<result>
+            <channel>$($DC.Caption) LogStatus</channel>
+            <value>$($dclogstatus)</value>
+            <ValueLookup>prtg.datacore.logstatus</ValueLookup>
+            </result>"
+
+        #PowerState
+        switch ($DC.PowerState)
+            {
+            'Unknown' { $dcpowerstate = 1 }
+            'ACOffline' { $dcpowerstate = 2}
+            'ACOnline' { $dcpowerstate = 3}
+            'BatteryLow' { $dcpowerstate = 4}
+            default { $dcpowerstate = -1 }
+            }
+        $xmlOutput = $xmlOutput + "<result>
+            <channel>$($DC.Caption) PowerState</channel>
+            <value>$($dcpowerstate)</value>
+            <ValueLookup>prtg.datacore.powerstate</ValueLookup>
+            </result>"
+        
+        #CacheState
+        switch ($DC.CacheState)
+            {
+            'Unknown' { $dccachestate = 1 }
+            'WritethruGlobal' { $dccachestate = 2}
+            'WritebackGlobal' { $dccachestate = 3}
+            default { $dccachestate = -1 }
+            }
+        $xmlOutput = $xmlOutput + "<result>
+            <channel>$($DC.Caption) CacheState</channel>
+            <value>$($dccachestate)</value>
+            <ValueLookup>prtg.datacore.cachestate</ValueLookup>
+            </result>"
+
+        #System Memory %
+        #Total/Available
+        try{
+            $UsedMem = [math]::Round(($DC.AvailableSystemMemory.Value / $DC.TotalSystemMemory.Value *100),0)
+            }
+        catch
+            {
+            $UsedMem = 0
+            }
+        $xmlOutput = $xmlOutput + "<result>
+            <channel>$($DC.Caption) MemoryUsed</channel>
+            <value>$($UsedMem)</value>
+            <unit>Percent</unit>
+            <limitmode>1</limitmode>
+            <LimitMaxError>90</LimitMaxError>
+            <LimitMaxWarning>80</LimitMaxWarning>
+            </result>"
+        }
+
+    #Storage Used (Byte?)
+    $StorageUsed = $DCs.StorageUsed | Select-Object -First 1 -ExpandProperty Value
+    $xmlOutput = $xmlOutput + "<result>
+            <channel>Storage Used</channel>
+            <value>$($StorageUsed)</value>
+            <unit>BytesDisk</unit>
+            </result>"
+
+    if($DCsOnline -ge 1)
+        {
+        $xmlOutput = $xmlOutput + "<text>$($NodeOnlineTXT)</text>"
+        }
+    else
+        {
+        $xmlOutput = $xmlOutput + "<text>No Online Nodes!</text>"
+        }
+    }
+# End Region Datacore Status
+
+
+# Start Region Datacore VirtualDisks
+if($DcsVirtualDisks)
+    {
+    #All Virtual Disks
+    $disks = Get-DcsVirtualDisk -Connection $con
+
+    #Online/Offline
+    $NotOnlineText = "Disk not Online: "
+    $DiskOnline = $disks | Where-Object {$_.diskstatus -eq  "online"}
+    $DiskNotOnline = $disks | Where-Object {$_.diskstatus -ne  "online"}
+    Foreach ($Disk in $DiskNotOnline )
+        {
+        $NotOnlineText += "$($Disk.Caption) - $($Disk.DiskStatus); "
+        }
+
+
+    #Not MultiPathMirrored
+    $NonMirrorText = "Disk NonMirrored: "
+    $NonMirrored = $diskonline | Where-Object {$_.Type -ne  "MultiPathMirrored"}
+    Foreach ($Disk in $NonMirrored )
+        {
+        $NonMirrorText += "$($Disk.Caption); "
+        }
+
+    #RecoveryState
+    $RecoveryText = "Disk Recovery: "
+    $RecoveryNeeded = Get-DcsMirror -Connection $con | Where-Object {(($_.FirstRecovery -ne "NoRecoveryNeeded") -and ($_.FirstRecovery -ne  "Undefined")) -or (($_.SecondRecovery -ne "NoRecoveryNeeded") -and ($_.SecondRecovery -ne "Undefined"))}
+    Foreach ($Disk in $RecoveryNeeded )
+        {
+        if(($Disk.FirstRecovery -ne "NoRecoveryNeeded") -and ($Disk.FirstRecovery -ne  "Undefined"))
+            {
+            $RecoveryText += "$($Disk.Caption) - $($Disk.FirstRecovery); "
+            }
+        else
+            {
+            $RecoveryText += "$($Disk.Caption) - $($Disk.SecondRecovery); "
+            }
+        
+        }
+
+    #Output Text
+    if($DiskNotOnline.Count -gt 0)
+        {
+        $OutputText += $NotOnlineText
+        }
+
+    if($NonMirrored.Count -gt 0)
+        {
+        $OutputText += $NonMirrorText
+        }
+
+    if($RecoveryNeeded.Count -gt 0)
+        {
+        $OutputText += $RecoveryText
+        }
+
+    if($OutputText -ne "")
+        {
+        $xmlOutput = $xmlOutput + "<text>$($OutputText)</text>"
+        }
+
+    #Graphs
+    $xmlOutput = $xmlOutput + "<result>
+            <channel>VDisks Online</channel>
+            <value>$($DiskOnline.Count)</value>
+            <unit>Count</unit>
+            </result>
+            <result>
+            <channel>VDisks Not Online</channel>
+            <value>$($DiskNotOnline.Count)</value>
+            <unit>Count</unit>
+            <limitmode>1</limitmode>
+            <LimitMaxError>0</LimitMaxError>
+            </result>
+            <result>
+            <channel>VDisks NonMirrored</channel>
+            <value>$($NonMirrored.Count)</value>
+            <unit>Count</unit>
+            <limitmode>1</limitmode>
+            <LimitMaxWarning>0</LimitMaxWarning>
+            </result>
+            <result>
+            <channel>VDisks Recovery needed</channel>
+            <value>$($RecoveryNeeded.Count)</value>
+            <unit>Count</unit>
+            <limitmode>1</limitmode>
+            <LimitMaxError>0</LimitMaxError>
+            </result>"
+    }
+# End Region Datacore VirtualDisks
+
+
+# Start Region Datacore Pools
+if($DcsPools)
+    {
+    # Get DataCore Server
+    $Pools = Get-DcsPool -Connection $con
+
+    # For Each Pool
+    foreach ($Pool in $Pools) 
+        {
+        #PoolStatus
+        switch ($Pool.PoolStatus)
+            {
+            'Running' { $dcPoolStatus = 1 }
+            'Initializing' { $dcPoolStatus = 2}
+            'MissingDisks' { $dcPoolStatus = 3}
+            'Foreign' { $dcPoolStatus = 4}
+            'Offline' { $dcPoolStatus = 5}
+            'Unknown' { $dcPoolStatus = 6}
+            default { $dcPoolStatus = -1 }
+            }
+        $xmlOutput = $xmlOutput + "<result>
+            <channel>$($Pool.Caption) PoolStatus</channel>
+            <value>$($dcPoolStatus)</value>
+            <ValueLookup>prtg.datacore.poolstatus</ValueLookup>
+            </result>"
+
+        #PresenceStatus
+        switch ($Pool.PresenceStatus)
+            {
+            'Unknown' { $dcPresenceStatus = 1 }
+            'Present' { $dcPresenceStatus = 2}
+            'NotPresent' { $dcPresenceStatus = 3}
+            default { $dcPresenceStatus = -1 }
+            }
+        $xmlOutput = $xmlOutput + "<result>
+            <channel>$($Pool.Caption) PresenceStatus</channel>
+            <value>$($dcPresenceStatus)</value>
+            <ValueLookup>prtg.datacore.presencestatus</ValueLookup>
+            </result>"
+
+        $perf = Get-DcsPerformanceCounter -Object $Pool -Connection $con
+        $SpaceFree = [math]::Round($perf.PercentAvailable,0)
+        $xmlOutput = $xmlOutput + "<result>
+            <channel>$($Pool.Caption) SpaceFree</channel>
+            <value>$($SpaceFree)</value>
+            <unit>Percent</unit>
+            <limitmode>1</limitmode>
+            <LimitMinError>10</LimitMinError>
+            <LimitMinWarning>20</LimitMinWarning>
+            </result>"
+        }
+    }
+# End Region Datacore Pools
+
+# Start Region Datacore Pools
+if($DcsPorts)
+    {
+    $Ports = Get-DcsPortConnection -Connection $con
+
+    $NotConnected = ""
+    $NotConnectedCount = 0
+    $NotPresent = ""
+    $NotPresentCount = 0
+    # For Each Pool
+    foreach ($Port in $Ports) 
+        {
+        #Connected?
+        if($Port.Connected -eq $false)
+            {
+            $NotConnectedCount += 1
+            $NotConnected += "$($Port.ExtendedCaption) not connected; "
+
+            }
+
+        #Present?
+        elseif(($Port.Connected -eq $true) -and ($Port.Present -eq $false))
+            {
+            $NotPresentCount += 1
+            $NotPresent += "$($Port.ExtendedCaption) is connected but not Present; "
+            }    
+        }
+
+    $xmlOutput = $xmlOutput + "<result>
+            <channel>Ports not Connected</channel>
+            <value>$($NotConnectedCount)</value>
+            <unit>Count</unit>
+            <limitmode>1</limitmode>
+            <LimitMaxError>0</LimitMaxError>
+            </result>
+            <result>
+            <channel>Ports not Presented</channel>
+            <value>$($NotPresentCount)</value>
+            <unit>Count</unit>
+            <limitmode>1</limitmode>
+            <LimitMaxWarning>0</LimitMaxWarning>
+            </result>"
+
+    $OutputText = ""
+
+    if($NotConnectedCount -gt 0)
+        {
+        $OutputText += $NotConnected
+        }
+
+    if($NotPresentCount -gt 0)
+        {
+        $OutputText += $NotPresent
+        }
+
+
+    if(($NotConnectedCount -gt 0) -or ($NotPresentCount -gt 0))
+        {
+        $OutputText = $OutputText.Replace("<","")
+        $OutputText = $OutputText.Replace(">","")
+        $OutputText = $OutputText.Replace("#","")
+        #The number sign (#) is not supported in sensor messages. If a message contains a number sign, the message is clipped at this point - https://www.paessler.com/manuals/prtg/custom_sensors
+        
+        $xmlOutput = $xmlOutput + "<text>$($OutputText)</text>"
+        }
+
+    else
+        {
+        $xmlOutput = $xmlOutput + "<text>All Ports Connected</text>"
+        }
+    }
+# End Region Datacore Ports
 
 # Disconnect DataCore
 Disconnect-DcsServer -Connection $con
-
-# Output Text
-$OutputText =""
-
-if($ErrorCount -gt 0)
-    {
-    $OutputText += "Error: $($ErrorText)"
-    }
-
-if($WarningCount -gt 0)
-    {
-    $OutputText += "Warning: $($WarningText)"
-    }
-
-
-if(($WarningCount -gt 0) -or ($ErrorCount -gt 0))
-    {
-    $OutputText = $OutputText.Replace("<","")
-    $OutputText = $OutputText.Replace(">","")
-    $OutputText = $OutputText.Replace("#","")
-    #The number sign (#) is not supported in sensor messages. If a message contains a number sign, the message is clipped at this point - https://www.paessler.com/manuals/prtg/custom_sensors
-    
-    $xmlOutput = $xmlOutput + "<text>$OutputText</text>"
-    }
-
-else
-    {
-    $xmlOutput = $xmlOutput + "<text>No Alerts found</text>"
-    }
 
 $xmlOutput = $xmlOutput + "</prtg>"
 
